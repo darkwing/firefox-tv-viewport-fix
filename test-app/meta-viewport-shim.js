83b6ed9ef3dc0d5965078c5b391044cfbdf51428
David Walsh <davidwalsh83@gmail.com>
2016-03-08 13:55:52 -0600

    Testing...

--- test-app/meta-viewport-shim.js
+++ test-app/meta-viewport-shim.js
@@ -1,18 +1,32 @@
-function metaViewportShim(container, showReport) {
+function metaViewportShim(showReport) {
   // The "container" argument is the element that houses the entire app
   // And sets its width/height to a standard TV size
   // The "showReport" argument is a boolean as to whether or not to show scaling information
+
+  var container = document.body;
+  var containerWidth = container.clientWidth;
+  var containerHeight = container.clientHeight;
+
+  // Start by looking for a meta viewport tag to see the desired size
+  var desiredViewportWidth = (function() {
+    var metaViewport = document.querySelector('meta[name=viewport]');
+    if(metaViewport) {
+      var matches = metaViewport.getAttribute('content').match('width=([0-9]+|device\-width)');
+      if(matches && matches[1]) {
+        if(String(matches[1]).toLowerCase() !== 'device-width') {
+          return Number(matches[1]);
+        }
+      }
+    }
+    return false;
+  })();
 
   // Get available dimensions of device
   var availableWidth = window.innerWidth;
   var availableHeight = window.innerHeight;
 
-  // `getBoundingClientRect` gives us dimensions after transformations
-  var containerWidth = container.getBoundingClientRect().width;
-  var containerHeight = container.getBoundingClientRect().height;
-
   // Calculates an approximate scale factor for a needed transform
-  var scaleFactor = (availableWidth / containerWidth).toFixed(2);
+  var scaleFactor = (availableWidth / desiredViewportWidth).toFixed(2);
 
   var performedScale = false;
   if(scaleFactor != '1.00') {
@@ -30,6 +44,7 @@ function metaViewportShim(container, showReport) {
     report.innerHTML = [
       'Available width: ' + availableWidth,
       'Container width: ' + containerWidth,
+      'Desired width: ' + desiredViewportWidth,
       'Available height: ' + availableHeight,
       'Container height: ' + containerHeight,
       'Scale Factor: ' + scaleFactor,
